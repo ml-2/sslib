a85a57216efdde7585d95a54fe46bff1951d8075
ML <tsild@kolabnow.com>
2019-09-02 13:22:22 -0400

    Bump up version, use latest POI version

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject sslib "0.3.0-SNAPSHOT"
+(defproject sslib "0.4.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "GNU General Public License version 3"
             :url ""} ;; TODO
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [dataorigin/dataorigin "0.1.0-SNAPSHOT"]
-                 [org.apache.poi/poi "3.17"]
-                 [org.apache.poi/poi-ooxml "3.17"]]
+                 [org.apache.poi/poi "4.1.0"]
+                 [org.apache.poi/poi-ooxml "4.1.0"]]
   :aot [sslib.core])
