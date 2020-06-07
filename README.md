b7e12d8c0d6a41be623324e89fa097f3ddbe9f3c
ML <tsild@kolabnow.com>
2020-06-07 15:50:23 -0400

    Add a readme and a license

---
+++ README.md
@@ -0,0 +1,3 @@
+# About
+
+A library for interacting with xls and xlsx files via Apache POI. This library was made for my own personal use, and is not intended to be used by others.
--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject sslib "0.5.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "GNU General Public License version 3"
-            :url ""} ;; TODO
+  :description "Library for interacting with xls and xlsx files via Apache POI"
+  :url ""
+  :license {:name "GNU General Public License version 3 or later, with Clojure exception"
+            :url ""}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [dataorigin/dataorigin "0.1.0-SNAPSHOT"]
                  [org.apache.poi/poi "4.1.0"]
